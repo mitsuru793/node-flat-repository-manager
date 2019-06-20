0068ba3648990fef2a4ad76a1824d8504f256740
mitsuru793 <mitsuru793@gmail.com>
2019-06-20 21:39:52 +0900

    test: fix temp dir path

--- src/__tests__/command/listCommandTest.ts
+++ src/__tests__/command/listCommandTest.ts
@@ -1,25 +1,30 @@
-import { execSync } from 'child_process'
-import { mkdirSync } from 'fs'
+import {execSync} from 'child_process'
+import {mkdirSync} from 'fs'
 import tmpDir = require('temp-dir')
 import rimraf = require('rimraf')
 
+const root = `${tmpDir}/projects`
+
 function cli(args: string = ''): string {
-  const cmd = `NODE_ENV=debug ts-node ${__dirname}/../../index.ts --projectsRoot ${tmpDir} ${args}`
+  const cmd = `NODE_ENV=debug ts-node ${__dirname}/../../index.ts --projectsRoot ${root} ${args}`
   return execSync(cmd).toString()
 }
 
 describe('list command', () => {
   beforeEach(() => {
-    rimraf.sync(`${tmpDir}/*`)
+    rimraf.sync(root)
+    mkdirSync(root)
 
     const projectDirs = ['php-hello', 'php-world', 'node-hi']
     projectDirs.forEach(p => {
-      mkdirSync(`${tmpDir}/${p}`)
+      mkdirSync(`${root}/${p}`)
     })
   })
 
   it('lists success', async () => {
+    expect(tmpDir).toBeTruthy()
     const out = cli('list')
     expect(out).toMatchSnapshot()
   })
 })
+
